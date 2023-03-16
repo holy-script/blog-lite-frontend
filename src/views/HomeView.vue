240016b08c5c7df92b9f00ade3936d363f825801
Palash Johri <palashjohricool@gmail.com>
2023-03-16 18:11:46 +0530

    axios added, connected to flask api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts">
+<script lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+export default {
+  components: {
+    TheWelcome
+  },
+  setup() {
+    onMounted(async () => {
+      console.log((await axios.get(import.meta.env.VITE_API_URL)).data)
+    })
+  }
+}
 </script>
 
 <template>
